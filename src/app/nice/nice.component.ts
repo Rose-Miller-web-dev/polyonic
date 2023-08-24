03305b86e95078603a4682d91b0369471f7dafa6
rose-miller-web-dev <rosemillerupworkfreelancer@gmail.com>
2023-08-24 14:41:30 -0400

    .

--- src/app/nice/nice.component.ts
+++ src/app/nice/nice.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppComponent } from '../app.component';
 import { StorageService, User } from '../storage.service';
 
 @Component({
